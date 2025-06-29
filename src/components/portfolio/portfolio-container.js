6d70122f33d97ddbbd7b1f89df33acf07f589dc5
Jennynova <jeniferfernandezares@gmail.com>
2025-06-29 18:27:44 +0200

    Mi segundo commit: cambios terminados

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,12 +3,32 @@ import React, { Component } from "react";
 import PortfolioItem from "../portfolio/portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                { title: "Quip" } , 
+                { title: "Eventbrite" } , 
+                { title: "Ministry Safe" } , 
+                { title: "SwingAway"} 
+            ]
+        };
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        })
+    }
+
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
-                <PortfolioItem/>
+                {this.portfolioItems()}
             </div>
         )
     }
