a8c24698db7fce637a5bd4a7816d42cf0bbd5b56
Jennynova <jeniferfernandezares@gmail.com>
2025-07-11 03:47:39 +0200

    formato imagenes

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationComponent from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -14,11 +12,9 @@ import NoMatch from "./pages/no-match";
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='container'>
         <Router>
           <div>
-            <h1>Jenifer Fernandez Portfolio</h1>
-            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationComponent />
 
             <Switch>
