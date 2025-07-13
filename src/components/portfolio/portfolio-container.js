0abc6f2f8580696630faed4fb38ed0a63934b980
Jennynova <jeniferfernandezares@gmail.com>
2025-07-13 11:09:57 +0200

    estilos y auth

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -52,17 +52,20 @@ export default class PortfolioContainer extends Component {
             return <div>Loading...</div>;
         }
 
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+    return (
+      <div className="portfolio-items-wrapper">
+        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-                <button onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
-                <button onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
-                <button onClick={() => this.handleFilter("Enterprise")}>Enterprise</button>
-
-                <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
-
-            </div>
-        )
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
